b73ee3c20a090708b16325327c469d296f81c9b9
Gunnar Beutner <gunnar@beutner.name>
2005-05-15 19:34:01 +0000

    no message
    git-svn-id: http://svn.beutner.name/trunk/sbnc@98 b3a6972a-4407-0410-a3f0-9bc6c1699d69

--- tickle.tcl
+++ tickle.tcl
@@ -1,14 +1,9 @@
 package require bnc 0.2
 
 internalbind server my_evil_proc
-internalbind pulse my_pulse_proc
 
 proc my_evil_proc {client parameters} {
 	set xyz [split [lindex $parameters 3]]
-
-	if {[string equal -nocase [lindex $parameters 1] "privmsg"] && [string equal -nocase $xyz "!hello"]} {
-		putserv "PRIVMSG [lindex $parameters 2] :Hello world from TCL!"
-	}
 
 	if {[lindex $parameters 1] == 475 && [string equal -nocase [lindex $parameters 3] "#illuminati"]} {
 		putserv "JOIN #illuminati fnords"
@@ -16,12 +11,5 @@ proc my_evil_proc {client parameters} {
 
 	if {[lindex $parameters 1] == 475 && [string equal -nocase [lindex $parameters 3] "#ee.intern"]} {
 		putserv "JOIN #ee.intern feedback"
-	}
-}
-
-proc my_pulse_proc {time} {
-	if {[expr $time % 3600] == 0} {
-		setctx "chaos"
-		putserv "PRIVMSG #illuminati :$time"
 	}
 }
