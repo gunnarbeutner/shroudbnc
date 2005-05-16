4704ad6e3bb1eaed06c1d451f8165df7d9e43bb4
Gunnar Beutner <gunnar@beutner.name>
2005-05-16 05:46:56 +0000

    no message
    git-svn-id: http://svn.beutner.name/trunk/sbnc@100 b3a6972a-4407-0410-a3f0-9bc6c1699d69

---
+++ md5util/StdAfx.h
@@ -0,0 +1,25 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__734081F9_EEA5_43E9_94FC_FF68120321D4__INCLUDED_)
+#define AFX_STDAFX_H__734081F9_EEA5_43E9_94FC_FF68120321D4__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern "C" {
+	#include "../md5-c/global.h"
+	#include "../md5-c/md5.h"
+}
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__734081F9_EEA5_43E9_94FC_FF68120321D4__INCLUDED_)
---
+++ md5util/md5util.cpp
@@ -0,0 +1,32 @@
+// md5util.cpp : Defines the entry point for the console application.
+//
+
+#include "StdAfx.h"
+
+int main(int argc, char* argv[]) {
+	if (argc < 2) {
+		fprintf(stderr, "md5util\nsyntax: md5util <string>\n");
+		return 1;
+	}
+
+	const char* String = argv[1];
+
+	MD5_CTX context;
+	static char Result[33];
+	unsigned char digest[16];
+	unsigned int len = strlen(String);
+
+	MD5Init (&context);
+	MD5Update (&context, (unsigned char*)String, len);
+	MD5Final (digest, &context);
+
+#undef sprintf
+
+	for (int i = 0; i < 16; i++) {
+		sprintf(Result + i * 2, "%02x", digest[i]);
+	}
+
+	printf("%s", Result);
+
+	return 0;
+}
