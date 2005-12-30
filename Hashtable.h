32ee4a036ac51df8471cc5c820e7c5356484db0b
Gunnar Beutner <gunnar@beutner.name>
2005-12-30 17:42:43 +0000

    git-svn-id: http://svn.beutner.name/trunk/sbnc@353 b3a6972a-4407-0410-a3f0-9bc6c1699d69

--- Hashtable.h
+++ Hashtable.h
@@ -17,6 +17,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. *
  *******************************************************************************/
 
+int keyStrCmp(const void *a, const void *b);
+
 template <typename Type> struct xhash_t {
 	char* Name;
 	Type Value;
