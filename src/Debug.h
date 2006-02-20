5ebb20e23fba9515d31cdfc08ce8bab2dd8c84eb
Gunnar Beutner <gunnar@beutner.name>
2006-02-20 09:11:20 +0000

    git-svn-id: http://svn.beutner.name/trunk/sbnc@551 b3a6972a-4407-0410-a3f0-9bc6c1699d69

---
+++ src/Debug.h
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * shroudBNC - an object-oriented framework for IRC                            *
+ * Copyright (C) 2005 Gunnar Beutner                                           *
+ *                                                                             *
+ * This program is free software; you can redistribute it and/or               *
+ * modify it under the terms of the GNU General Public License                 *
+ * as published by the Free Software Foundation; either version 2              *
+ * of the License, or (at your option) any later version.                      *
+ *                                                                             *
+ * This program is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ * GNU General Public License for more details.                                *
+ *                                                                             *
+ * You should have received a copy of the GNU General Public License           *
+ * along with this program; if not, write to the Free Software                 *
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. *
+ *******************************************************************************/
+
+typedef struct allocation_s {
+	const char *File;
+	unsigned int Size;
+	void *Pointer;
+} allocation_t;
+
+typedef struct file_s {
+	const char *File;
+	unsigned int AllocationCount;
+	unsigned int Bytes;
+} file_t;
+
+extern CVector<allocation_t> g_Allocations;
