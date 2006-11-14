02348ff811fccbe24a26aa3177392aaefe892750
Gunnar Beutner <gunnar@beutner.name>
2006-11-14 10:32:00 +0000

    git-svn-id: http://svn.beutner.name/trunk/sbnc@844 b3a6972a-4407-0410-a3f0-9bc6c1699d69

--- README.lean
+++ README.lean
@@ -8,8 +8,8 @@ When enabled the parser does not parse/process certain messages which are
 received from an IRC server. Several modes are defined:
 
 0 - default mode, every line is parsed as usual
-1 - chanusers' hosts are not updated
-2 - sbnc does not keep track of channel users at all
+1 - chanusers' hosts (i.e. ident@host) are not updated
+2 - sbnc does not keep track of channel users at all (i.e. information which the /names command returns)
 
 Using a TCL script you can set the "lean" mode for new users:
 
