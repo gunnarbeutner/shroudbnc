96fa7cde52495992c1e0021444c2e720fd87a2b7
Gunnar Beutner <gunnar@beutner.name>
2006-11-14 14:27:47 +0000

    git-svn-id: http://svn.beutner.name/trunk/sbnc@846 b3a6972a-4407-0410-a3f0-9bc6c1699d69

--- src/utility.h
+++ src/utility.h
@@ -162,7 +162,7 @@ typedef struct {
 #endif
 } mblock;
 
-#define BLOCKMARKER 0xCB3D33B8
+#define BLOCKMARKER 0xCB3D3AB8
 
 void *mmalloc(size_t Size, CUser *Manager);
 void *mrealloc(void *Block, size_t NewSize, CUser *Manager);
